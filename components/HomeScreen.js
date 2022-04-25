16fc118841c87dd54b4f8310deeb55c3dfa5d47a
Joosep Arrak <jooseparrak@gmail.com>
2022-04-25 23:02:51 +0300

    Home background v1.2

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -4,7 +4,7 @@ function HomeScreen() {
   return (
     <div
       id="home"
-      className="grid w-full h-screen place-items-center md:py-64 py-[50%]"
+      className="grid w-full h-screen place-items-center md:py-64 py-[50%] svg-bg"
     >
       <div className="w-2/3 2xl:pl-40 h-full">
         <motion.h1
