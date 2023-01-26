a9ab80ac285f4e1f87208149d9cfd3f47031bcc0
Joosep Arrak <jooseparrak@gmail.com>
2023-01-26 20:23:57 +0200

    Update Intro text color & add a background gradient

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -5,12 +5,12 @@ function Intro() {
     <section className="w-full h-screen flex items-center justify-center">
       <div className="select-none pointer-events-none">
         <Parallax speed={-100} translateY={[-20, 20]} translateX={[25, -25]}>
-          <h1 className="h1 gradient-primary text-6xl md:text-9xl font-bold text-center mb-4">
+          <h1 className="h1 gradient-secondary text-6xl md:text-9xl font-bold text-center mb-4">
             JOOSEP
           </h1>
         </Parallax>
         <Parallax speed={-500} translateY={[-200, 200]} translateX={[-25, 25]}>
-          <p className="gradient-primary text-2xl md:text-4xl font-medium text-center">
+          <p className="gradient-secondary text-2xl md:text-4xl font-medium text-center">
             Frontend Developer
           </p>
         </Parallax>
