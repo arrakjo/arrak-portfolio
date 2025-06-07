ffbc192e536c6aa99f60b849bb9b6ace52349e04
Joosep Arrak <joosep.arrak@pipedrive.com>
2025-06-08 01:18:18 +0300

    migrate to next.js v15

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -5,12 +5,12 @@ export default function HomeScreen() {
       className="grid w-full h-screen place-items-center md:py-64 py-[50%] svg-bg"
     >
       <div className="w-2/3 2xl:pl-40 h-full">
-        <h1 className="leading-none text-4xl md:text-5xl font-light">
-          <span className="font-medium">Hello,</span> I&apos;m
+        <h1 className="leading-none text-4xl md:text-5xl font-thin">
+          <span className="font-light">Hello,</span> I&apos;m
           <br />
-          <span className="font-light">Joosep Arrak</span>
+          <span className="font-thin">Joosep Arrak</span>
         </h1>
-        <h2 className="leading-none mt-[52px] text-3xl md:text-4xl font-medium">
+        <h2 className="leading-none mt-[52px] text-3xl md:text-4xl font-thin">
           Frontend
           <br />
           Software Engineer.
--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
 module.exports = {
-  nextConfig,
+  reactStrictMode: true,
   images: {
-    domains: ["ik.imagekit.io", "media.graphassets.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      {
+        protocol: "https",
+        hostname: "media.graphassets.com",
+      },
+    ],
   },
   i18n: {
     locales: ["en"],
